43c95dedd9d68afa26133c384470ce4da600bb09
CardenDang <dangcarden@gmail.com>
2026-02-13 10:24:20 -0800

    Add WordFreq class

--- src/main/java/wordfreq/WordFreq.java
+++ src/main/java/wordfreq/WordFreq.java
@@ -1,5 +1,15 @@
 package wordfreq;
 
+/**
+ * Stores a phrase and a frequency
+ * Higher frequency = higher priority
+ */
 public class WordFreq {
-    
+    String word;
+    int frequency;
+
+    public WordFreq(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
 }
